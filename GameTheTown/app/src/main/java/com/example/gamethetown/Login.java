84534be005d4c9533b54b958e44590d2848610cc
Bernardo <bernardoneves1996@gmail.com>
2017-11-09 17:00:04 +0000

    oi

--- GameTheTown/app/src/main/java/com/example/gamethetown/Login.java
+++ GameTheTown/app/src/main/java/com/example/gamethetown/Login.java
@@ -1,26 +1,12 @@
 package com.example.gamethetown;
 
 import android.content.Intent;
-import android.support.constraint.ConstraintLayout;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.util.DisplayMetrics;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.MenuInflater;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.PopupWindow;
-import android.widget.RelativeLayout;
 
 public class Login extends AppCompatActivity {
 
-    private PopupWindow popwindow;
-    private LayoutInflater layoutInflater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,29 +14,6 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
-    /**
-     * When the register button is pressed
-     */
-    /**
-    public void onRegisterPress(View view) {
-
-        DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
-        int width = dm.widthPixels;
-        int height = dm.heightPixels;
-
-        layoutInflater = (LayoutInflater) getApplicationContext()
-                .getSystemService(LAYOUT_INFLATER_SERVICE);
-        ViewGroup container = (ViewGroup) layoutInflater.inflate(R.layout.popregister,null);
-        //size
-        popwindow = new PopupWindow(container,height/2,width/2,true);
-        //position
-        popwindow.showAtLocation((ConstraintLayout)findViewById(R.id.login),
-                Gravity.NO_GRAVITY,0,0);
-
-        //startActivity(new Intent(this,Pop.class));
-    }
-    **/
     /**
      * Start user query
      * if query is successfull login
